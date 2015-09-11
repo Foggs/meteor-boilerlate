8bc5b026459fbcb06b8cf7940dff09237f109751
= <robfoggo@gmail.com>
2015-09-10 21:15:19 -0400

    login functionality added

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,14 +1,23 @@
 Session.setDefault('counter', 0);
 
 Template.hello.helpers({
-  counter: function () {
-    return Session.get('counter');
-  }
+    comments: function () {
+        return data.find();
+    },
+    
+    counter: function () {
+        return Session.get('counter');
+    }
 });
 
 Template.hello.events({
-  'click button': function () {
-    // increment the counter when button is clicked
-    Session.set('counter', Session.get('counter') + 1);
-  }
+    'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+        data.insert({text:'This is comment #' + (data.find().count()+1)});
+    }
+});
+
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
